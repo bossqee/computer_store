796c092c12df719c3de18c1e3309316ffcf22877
bossqee <p30675130@gmail.com>
2026-08-29 20:30:59 +0700

    logo-brand

--- application/views/Home.php
+++ application/views/Home.php
@@ -25,6 +25,37 @@
     <p>ช้อปสะดวกได้ตลอด 24 ชั่วโมง พร้อมบริการหลังการขายและให้คำปรึกษาฟรีโดยทีมงานมืออาชีพ ช้อปสบายใจ มั่นใจ ได้ของชัวร์ที่ PC4U</p>
 </div>
 
+<div class="logo-brand">
+    <h3>สินค้าจากแบรนด์ชั้นนำ</h3>
+    <div class="logo-brand-container">
+        <img src="<?php echo base_url('assets/img/logo-brand/acer.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/amd.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/aoc.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/asrock.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/asus.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/corsair.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/dahua.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/ergonoz.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/fantech.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/gigabyte.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/hiksemi.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/hp.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/hyperx.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/inno3d.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/intel.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/keychron.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/kingston.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/lenovo.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/lexar.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/lg.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/loga.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/logitech.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/microsoft.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/msi.png') ?>" alt="logo">
+        <img src="<?php echo base_url('assets/img/logo-brand/nubwo.png') ?>" alt="logo">
+    </div>
+</div>
+
 <?php $this->load->view('component/Footer.php') ?>
 
 <script>
